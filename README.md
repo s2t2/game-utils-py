883f2af1c39f12f81d93f161e4bffb497eebdeb7
Mike Rossetti <s2t2@users.noreply.github.com>
2019-05-30 15:33:49 -0400

    Add tests

--- README.md
+++ README.md
@@ -4,10 +4,23 @@
 
 ## Installation
 
-Install from PyPI:
+Install the package using one of the following commands, as desired:
 
 ```sh
+# from PyPI (recommended):
 pip install s2t2-game-utils
+
+# from PyPI Test server:
+pip install -i https://test.pypi.org/simple/ s2t2-game-utils
+
+# from GitHub source (HTTPS version):
+pip install git+https://github.com/s2t2/game-utils-py.git
+
+# from GitHub source (SSH version):
+pip install git+ssh://git@github.com/s2t2/game-utils-py.git
+
+# from local source (after downloading the repo):
+pip install -e path/to/game-utils-py/
 ```
 
 ## Usage
---
+++ test/rock_paper_scissors_test.py
@@ -0,0 +1,20 @@
+from game_utils.rock_paper_scissors import random_choice, determine_winner
+
+def test_random_choice():
+    assert random_choice() in ["rock", "paper", "scissors"]
+
+    custom_options = [1, 5, 7]
+    assert random_choice(custom_options) in custom_options
+
+def test_determination_of_the_winner():
+    assert determine_winner("rock", "rock") == None
+    assert determine_winner("rock", "paper") == "paper"
+    assert determine_winner("rock", "scissors") == "rock"
+
+    assert determine_winner("paper", "rock") == "paper"
+    assert determine_winner("paper", "paper") == None
+    assert determine_winner("paper", "scissors") == "scissors"
+
+    assert determine_winner("scissors", "rock") == "rock"
+    assert determine_winner("scissors", "paper") == "scissors"
+    assert determine_winner("scissors", "scissors") == None
